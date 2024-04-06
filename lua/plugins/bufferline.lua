40517be604e22e5da5189e8bf851c9e8b82f5276
ludoboulanger <ludovic.boulanger@outlook.com>
2024-04-06 08:24:07 -0400

    Remove commented out commands

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,14 +2,8 @@ return {
 	"akinsho/bufferline.nvim",
 	version = "*",
 	dependencies = "nvim-tree/nvim-web-devicons",
-    --keys = {
-    --  { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
-    --  { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
-    --  { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
-    --  { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
-    --},
 	config = function()
-		vim.opt.termguicolors = true
+        vim.opt.termguicolors = true
         vim.api.nvim_set_keymap("n", "<S-h>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
         vim.api.nvim_set_keymap("n", "<S-l>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
 		local opts = {
