326c4caa105cca04d4944f2926e7414b54c9c8e9
Ludovic Boulanger <ludovic.boulanger@outlook.com>
2024-04-27 20:00:53 -0400

    Lazy Git integration to toggle term

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -12,6 +12,15 @@ return {
             },
             close_on_exit = true,
             shell = "bash",
+
         })
+        local Terminal = require('toggleterm.terminal').Terminal
+        local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true })
+
+        function _lazygit_toggle()
+            lazygit:toggle()
+        end
+
+        vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
     end
 }
